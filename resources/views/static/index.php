8cd461d24ae8f217ca4a18c703f5504f48bb6758
Miguel Angel Garcia <miguel@miguelangelgarcia.com>
2018-02-18 22:52:24 -0600

    html updates

--- resources/views/static/index.php
+++ resources/views/static/index.php
@@ -1,31 +1,25 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js">
-<!--<![endif]-->
+<!doctype html>
+<html class="no-js" lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Miguel Garcia</title>
-    <meta name="description" content="Building Beautiful Hand Crafted Web and Mobile Experiences">
+    <meta name="description" content="Building Beautiful Web and Mobile Experiences">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/app.min.css">
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700|Montserrat:700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700|Montserrat:700' rel='stylesheet' type='text/css'>
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
 </head>
 <body>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-371720-1"></script>
     <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-371720-1', 'auto');
-        ga('send', 'pageview');
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-371720-1');
     </script>
+
     <div id="home">
         <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container">
@@ -39,40 +33,38 @@
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#about_me">About Me</a></li>
-                        <li><a href="#experience">Experience</a></li>
-                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#home" title="Home">Home</a></li>
+                        <li><a href="#about_me" title="About Me">About Me</a></li>
+                        <li><a href="#experience" title="Experience">Experience</a></li>
+                        <li><a href="#portfolio" title="Portfolio">Portfolio</a></li>
                     </ul>
                 </div> <!--/.nav-collapse -->
             </div> <!-- /.container -->
         </nav>
 
-        <div id="intro">
-            <h1>Hello!<br />I'm <span>Miguel Angel Garcia</span><span class="subtitle">Web Developer</span></h1>
-            <p>Building Beautiful Hand Crafted Web and Mobile Experiences</p>
-        </div>
-
-        <div id="slides">
-            <div class="slides-container">
-                <img src="/images/web-artisan1.jpg" alt="Mayan Developer">
+        <div id="hero">
+            <div id="intro">
+                <h1><span>Hello!</span><br />I'm Miguel Angel Garcia<span class="subtitle">Web Developer</span></h1>
+                <p>Building Beautiful Web and Mobile Experiences, living in Dallas, Texas</p>
             </div>
-        </div><!-- /#slides -->
+        </div>
 
         <section id="about_me" class="wrap">
             <div class="container about-me">
-                <h2>About Me</h2>
+                <h2 title="About Miguel Garcia">About Me</h2>
                 <p class="quote"><q>Stay hungry, stay foolish.</q> - Steve Jobs</p>
                 <div class="row">
                     <div class="col-sm-4 col-sm-push-8"><img src="/images/miguel-garcia.jpg" alt="Miguel Angel Garcia" class="img-responsive"><br /><br /></div>
-                    <div class="col-sm-8 col-sm-pull-4">
+                    <div class="col-sm-8 col-sm-pull-5">
                         <h3>Miguel Angel Garcia</h3>
-                        <h4>Never. Stop. Learning.</h4>
+
                         <p>I'm a Web Developer and my passion is to engineer the best possible web experiences for web and mobile users. I my 10 years of experience I had the opportunity and dedication to learn several technologies and programming languages. I have worked with many talented developers and designers and there is always something new to learn especially from creative talented people in this industry.</p>
 
-                        <p>I have eye for beautiful interface design with emphasis on user experience. Over the years I have developed dynamic and full interactive websites, web and mobile applications by using a variety of languages and frameworks. I also have developed my own custom content management system that is fully responsive.</p>
+                        <p>I have eye for beautiful interface design with emphasis on user experience. Over the years I have developed dynamic and full interactive websites, web and mobile applications by using a variety of languages and frameworks.</p>
 
                         <p>As a web developer I seek a leadership position to be a part of an intelligent and creative team where I can contribute to its growth continue to utilize and improve my skills.</p>
+
+                        <h4>Never. Stop. Learning.</h4>
 
                     </div>
 
@@ -106,8 +98,8 @@
                             <li>2 iPads</li>
                             <li>4 iPhones</li>
                             <li>1 HTC EVO 3D</li>
-                            <li>2861+ Cups Of Coffee</li>
-                            <li>348650+ Lines Of Code</li>
+                            <li>3161+ Cups Of Coffee</li>
+                            <li>378650+ Lines Of Code</li>
                         </ul>
                     </div>
                 </div>
@@ -119,7 +111,33 @@
                 <h2>Experience</h2>
                 <p class="quote"><q>To turn really interesting ideas and fledgling technologies into a company that can continue to innovate for years, it requires a lot of disciplines.</q> - Steve Jobs</p>
 
-                <div class="row">
+                <div class="row technologies">
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/html5.svg" alt="HTML5" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/css3.svg" alt="CSS3" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/git.svg" alt="Git" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/gulpjs.svg" alt="Gulpjs" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/bootstrap-4.svg" alt="Bootstrap 4" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/bower.svg" alt="Bower" class="img-responsive"></div></div>
+                </div>
+                <div class="row technologies">
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/javascript.svg" alt="JavaScript" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/webpack.svg" alt="Webpack" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/nodejs.svg" alt="NodeJS" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/vuejs.svg" alt="Vuejs" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/greensock.svg" alt="GreenSock" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/jquery.svg" alt="JQuery" class="img-responsive"></div></div>
+                </div>
+                <div class="row technologies">
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/mysql.svg" alt="MySQL" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/php.svg" alt="PHP" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/redis.svg" alt="Redis" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/composer.svg" alt="Composer" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/laravel.svg" alt="Laravel" class="img-responsive"></div></div>
+                    <div class="col-sm-2"><div class="icon"><img src="/images/icons/terminal.svg" alt="Terminal" class="img-responsive"></div></div>
+
+                </div>
+
+                <!-- <div class="row">
                     <div class="col-sm-12">
                         <div class="technologies">
                             <img src="/images/html5.png" class="img-responsive">
@@ -133,7 +151,7 @@
                             <i class="icon-html5_perfintegration"></i>
                         </div>
                     </div>
-                </div>
+                </div> -->
 
                 <div class="row">
                     <div class="col-sm-4">
@@ -151,6 +169,20 @@
                 </div>
                 <div class="row">
                     <div class="col-sm-4">
+                        <h3>Vue.js</h3>
+                        <p>Vue.js is an open-source progressive JavaScript framework for building user interfaces. In addition, vue can also function as a web application framework capable of powering advanced &nbsp;<nobr>single-page</nobr>&nbsp; applications.</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <h3>Webpack</h3>
+                        <p>Webpack is a bundler and it comes in handy when you need to pack multiple assets (images, fonts, etc.) together into a dependency graph.</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <h3>Gulp</h3>
+                        <p>It is a task runner built on Node.js and npm, used for automation of time-consuming and repetitive tasks involved in web development like minification, concatenation, cache busting, unit testing, linting, optimization, etc.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
                         <h3>PHP</h3>
                         <p>PHP is a server-side scripting language designed for web development but also used as a general-purpose programming language.</p>
                     </div>
@@ -163,7 +195,11 @@
                         <p>Laravel is a free, open source PHP web application framework, designed for the development of model–view–controller (MVC) web applications.</p>
                     </div>
                 </div>
-                <div class="row">
+                <!-- <div class="row">
+                    <div class="col-sm-4">
+                        <h3>Command Line Interface (CLI)</h3>
+                        <p>Is a text-based interface that is used to operate software and operating systems while allowing the user to respond to visual prompts by typing single commands into the interface and receiving a reply in the same way.</p>
+                    </div>
                     <div class="col-sm-4">
                         <h3>ACTIONSCRIPT 3.0</h3>
                         <p>ActionScript is an object-oriented language developed by Adobe. You can build things like games and Rich Internet Application (RIA).</p>
@@ -176,7 +212,7 @@
                         <h3>AFTER EFFECTS</h3>
                         <p>Adobe After Effects is primarily used for creating motion graphics and visual effects by allowing users to animate, alter and composite media in 2D and 3D space.</p>
                     </div>
-                </div>
+                </div> -->
             </div> <!-- /.container .experiance -->
         </section>
 
@@ -195,10 +231,38 @@
 
                 <div class="isotope row">
                     <div class="item col-sm-4 application">
+                        <img src="/images/portfolio/thumb/realpage-marketing-dashboard.jpg" class="img-responsive">
+                        <div class="item-text">
+                            <h3>RealPage CMS</h3>
+                            <p>App by: RealPage Inc.</p>
+                        </div>
+                    </div>
+                    <div class="item col-sm-4 website">
+                        <img src="/images/portfolio/thumb/realpage.jpg" class="img-responsive">
+                        <div class="item-text">
+                            <h3>RealPage</h3>
+                            <p>Site by: RealPage Inc. | <a href="https://www.realpage.com" target="_blank" class="btn btn-launch"><i class="icon-rocket"></i> Launch</a></p>
+                        </div>
+                    </div>
+                    <div class="item col-sm-4 website">
+                        <img src="/images/portfolio/thumb/kigo.jpg" class="img-responsive">
+                        <div class="item-text">
+                            <h3>Kigo</h3>
+                            <p>Site by: RealPage Inc. | <a href="https://www.kigo.net" target="_blank" class="btn btn-launch"><i class="icon-rocket"></i> Launch</a></p>
+                        </div>
+                    </div>
+                    <div class="item col-sm-4 website">
+                        <img src="/images/portfolio/thumb/propertyware.jpg" class="img-responsive">
+                        <div class="item-text">
+                            <h3>Propertyware</h3>
+                            <p>Site by: RealPage Inc. | <a href="https://www.propertyware.com" target="_blank" class="btn btn-launch"><i class="icon-rocket"></i> Launch</a></p>
+                        </div>
+                    </div>
+                    <div class="item col-sm-4 application">
                         <img src="/images/portfolio/thumb/freshthyme.jpg" class="img-responsive">
                         <div class="item-text">
                             <h3>Fresh Thyme CMS</h3>
-                            <p>App by: Ivie Inc. | <a href="http://freshthyme.com" target="_blank" class="btn btn-launch"><i class="icon-rocket"></i> Launch</a></p>
+                            <p>App by: Ivie Inc. | <a href="https://www.freshthyme.com/" target="_blank" class="btn btn-launch"><i class="icon-rocket"></i> Launch</a></p>
                         </div>
                     </div>
                     <div class="item col-sm-4 website">
@@ -303,15 +367,20 @@
 
         <section id="footer" class="bg-dark-blue">
             <div class="container footer">
-                Miguel Angel Garcia &copy; 2015
+                Miguel Angel Garcia &copy; <?php echo date('Y'); ?>
             </div>
         </section>
 
     </div><!-- /#wrap -->
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <!--[if lt IE 9]>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/js/vendor/jquery-2.2.4.min.js"><\/script>')</script>
+    <![endif]-->
+    <!--[if gte IE 9]><!-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+    <!--<![endif]-->
     <script src="/js/app.min.js"></script>
 </body>
 </html>
